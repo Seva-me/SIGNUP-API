c5d023945f0331c85258dd5aa792552360afd6fe
Seva-me <115927030+Seva-me@users.noreply.github.com>
2023-01-07 23:26:30 +0530

    Update studentrouter.js

--- controller/student/studentrouter.js
+++ controller/student/studentrouter.js
@@ -8,6 +8,6 @@ const validator = require('../../middlewares/validator');
 router.post('/insertstudentdata',validator(studentSchema.insertStudentData),studentControll.insertStudentData);
 router.put('/updatestudentdata',validator(studentSchema.updateStudentData),studentControll.updateStudent);
 router.get('/getStudentdetails',validator(studentSchema.studentDetail),studentControll.studentDetail);
-router.get('/studentDetails',validator(studentSchema.studentBySequelizeQuery),studentControll.studentDataBySequelizeQuery);
+router.get('/studentDetails',validator(studentSchema.studentDataBySequelizeQuery),studentControll.studentDataBySequelizeQuery);
 router.get('/courseDetails',validator(studentSchema.courseDetailsBySequelizeQuery),studentControll.courseDetailsBySequelizeQuery);
 module.exports=router
