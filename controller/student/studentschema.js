bfb3572a4b3ae470b5f4a11c5feb822c8a8801e3
Aanchal Maheshwari <maheshwari.aanchal@athansys.com>
2023-01-09 19:52:59 +0530

    to-git

--- controller/student/studentschema.js
+++ controller/student/studentschema.js
@@ -6,14 +6,9 @@ const insertStudent=joi.object().keys({
     age:joi.number().integer().min(5).max(20).required(),
     roll_no:joi.number().integer().required(),
     school_name:joi.string().pattern(new RegExp(pattern.namePattern)).required(),
-<<<<<<< HEAD
     blood_group:joi.string().valid('A+','B+','O+','O-','A-','B-','AB+','AB-').required(),
     address_id:joi.number().integer(),
     course_id: joi.number().integer(),
-=======
-    blood_group:joi.string().pattern(new RegExp(pattern.bloodGroupPattern)).required(),
-    address_id:joi.number().integer().required()
->>>>>>> 6ffce1751e27f23d18db62926239d9db744da657
 });
 
 const updateStudent=joi.object().keys({
@@ -23,11 +18,7 @@ const updateStudent=joi.object().keys({
     age:joi.number().integer().min(5).max(45),
     roll_no:joi.number().integer(),
     school_name:joi.string().pattern(new RegExp(pattern.namePattern)),
-<<<<<<< HEAD
     blood_group:joi.string().valid('A+','B+','O+','O-','A-','B-','AB+','AB-'),
-=======
-    blood_group:joi.string().pattern(new RegExp(pattern.bloodGroupPattern)),
->>>>>>> 6ffce1751e27f23d18db62926239d9db744da657
     address_id:joi.number().integer(),
     course_id: joi.number().integer(),
     id:joi.number().integer().required()
@@ -39,11 +30,7 @@ const getStudentList=joi.object().keys({
 });
 
 const studentDataBySequelizeQuery=joi.object().keys({
-<<<<<<< HEAD
     data:joi.string().pattern(new RegExp(pattern.dataPattern)).required(),
-=======
-    data:joi.string().pattern(new RegExp(dataPattern)).required(),
->>>>>>> 6ffce1751e27f23d18db62926239d9db744da657
     id:joi.number()
 });
 
@@ -53,15 +40,9 @@ const courseDetailsBySequelizeQuery=joi.object().keys({
 });
 
 module.exports={
-<<<<<<< HEAD
     insertStudent,
     updateStudent,
     getStudentList,
-=======
-    insertStudentData,
-    updateStudentData,
-    studentDetail,
->>>>>>> 6ffce1751e27f23d18db62926239d9db744da657
     studentDataBySequelizeQuery,
     courseDetailsBySequelizeQuery
     }
